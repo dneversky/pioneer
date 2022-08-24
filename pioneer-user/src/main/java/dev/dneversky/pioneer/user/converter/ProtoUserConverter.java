d309f4c1a0096d3931c6fdd001bca8afce2ca34f
Koshey <shdeemon@mail.ru>
2022-08-24 22:49:55 +0300

    nullable teamId processing

--- pioneer-user/src/main/java/dev/dneversky/pioneer/user/converter/ProtoUserConverter.java
+++ pioneer-user/src/main/java/dev/dneversky/pioneer/user/converter/ProtoUserConverter.java
@@ -3,13 +3,15 @@ package dev.dneversky.pioneer.user.converter;
 import dev.dneversky.pioneer.user.entity.User;
 import org.dneversky.gateway.UserServiceOuterClass;
 
+import java.util.Optional;
+
 public class ProtoUserConverter {
 
     public static UserServiceOuterClass.User convert(User user) {
         return UserServiceOuterClass.User.newBuilder()
                 .setId(user.getId())
                 .setNickname(user.getNickname())
-                .setTeamId(user.getTeamId())
+                .setTeamId(Optional.ofNullable(user.getTeamId()).orElse(""))
                 .addAllSpecsIds(user.getSpecs())
                 .build();
     }
