6181506bf01dfa833f4e1cbfcdf33f0ba994e81b
Koshey <shdeemon@mail.ru>
2022-08-27 23:23:46 +0300

    added eureka server registry to gateway service

--- pioneer-gateway/src/main/java/dev/dneversky/pioneer/gateway/PioneerGatewayApplication.java
+++ pioneer-gateway/src/main/java/dev/dneversky/pioneer/gateway/PioneerGatewayApplication.java
@@ -3,7 +3,9 @@ package dev.dneversky.pioneer.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @EnableEurekaClient
 @SpringBootApplication
 public class PioneerGatewayApplication {
