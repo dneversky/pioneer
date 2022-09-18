03844b6f4c02e800db7c560d98606cb00d3d54a6
Koshey <shdeemon@mail.ru>
2022-09-18 20:47:48 +0300

    removed gRPC from the project

--- pioneer-gateway/src/main/java/dev/dneversky/pioneer/gateway/exception/ExceptionControllerAdvice.java
+++ pioneer-gateway/src/main/java/dev/dneversky/pioneer/gateway/exception/ExceptionControllerAdvice.java
@@ -1,7 +1,5 @@
 package dev.dneversky.pioneer.gateway.exception;
 
-import io.grpc.Metadata;
-import io.grpc.StatusRuntimeException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -14,14 +12,6 @@ import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ExceptionControllerAdvice {
-
-    @ExceptionHandler(StatusRuntimeException.class)
-    public ResponseEntity<?> handleStatusRuntimeException(StatusRuntimeException e) {
-        Metadata trailers = e.getTrailers();
-        String statusCode = trailers.get(Metadata.Key.of("status_code", Metadata.ASCII_STRING_MARSHALLER));
-        String message = trailers.get(Metadata.Key.of("message", Metadata.ASCII_STRING_MARSHALLER));
-        return new ResponseEntity<>(message, HttpStatus.valueOf(statusCode));
-    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
--- pioneer-project/src/main/java/dev/dneversky/pioneer/project/api/ProjectGrpcService.java
+++
@@ -1,4 +0,0 @@
-package dev.dneversky.pioneer.project.api;
-
-public class ProjectGrpcService {
-}
